a0ac17da81a069a2e767f363fcfde10d5faf634e
ksimali <cassim.ali@outlook.fr>
2024-01-31 15:25:48 -0500

    Setting up main Frame: -Creation de la fenetre av JFrame Class -Config de la fenetre avec setDefault... & setSize method -Affichage de la fenetre avec setVisible

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        // Creation de la fenetre principale
+        JFrame frame  = new JFrame("Counter");
+
+
+
+        //Set up
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 240);
+
+        //Affichage
+        frame.setVisible(true);
     }
 }
